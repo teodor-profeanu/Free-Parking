ccd8e1a53f29a070ea246abe33acf3342ef26621
Teodor Profeanu <teodor.profeanu@gmail.com>
2023-12-10 04:44:53 +0200

    lesgooooooooo

--- app/src/main/java/com/example/polihi/MainActivity.kt
+++ app/src/main/java/com/example/polihi/MainActivity.kt
@@ -29,21 +29,6 @@ private lateinit var binding: ActivityMainBinding
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
-        val listView = findViewById<ListView>(R.id.lessonsListView)
-        val items = ArrayList<ListItem>()
-        items.add(ListItem(R.drawable.thumb1, "Language Basics - How to learn Them", "An in depth look at how\n...", "$"))
-        items.add(ListItem(R.drawable.thumb2, "English Language Learning", "Trying to learn english?\n...", ""))
-// Add more items...
-
-// Add more items...
-        val adapter = CustomAdapter(this, items)
-        listView.adapter = adapter
     }
 override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
